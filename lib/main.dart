2249b4b012276bc33b05bdd0eb2db671128219ab
Valery Borisenko <valera.borisencko@yandex.ru>
2025-06-18 23:38:09 +0300

    Переменные и ИМТ калькулятор

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bmi_calculator.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,29 +9,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[50],
-        appBar: AppBar(
-          title: const Text('Мой Flutter Путь'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Привет, Я - будущий Flutter разработчик!',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              Image.asset('assets/2024-10-07T19-18-41.jpg', width: 150),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Начать путешествие'),
-              ),
-            ],
-          ),
+      title: 'Мой Flutter Путь',
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Мой Flutter путь'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Привет, Я - будущий Flutter разработчик!',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Image.asset('assets/2024-10-07T19-18-41.jpg', width: 150),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                // Навигация на BmiCalculator
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const BmiCalculator()),
+                );
+              },
+              child: const Text('Начать путешествие'),
+            ),
+          ],
         ),
       ),
     );
